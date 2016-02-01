b36cbfc22ee247a0d67832d73062d70b687b35ad
dacosta_rafael <dacosta_rafael@hotmail.com>
2016-02-01 14:33:48 +0000

    modified  file to django projectone/appone

--- projectone/projectone/settings.py
+++ projectone/projectone/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'dacostarafael2',        # Or path to database file if using sqlite3.
         'USER': 'dacostarafael2',                      # Not used with sqlite3.
-        'PASSWORD': 'Doismile15@',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'dacostarafael2.db.10306788.hostedresource.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
